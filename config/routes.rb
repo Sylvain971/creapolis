97dc4d3d27e5709943b597e361174305837e7f83
Justine <>
2018-03-13 12:12:17 +0100

    added restriction to view index to authenticated users only

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-	get "users/show" #A supprimer quand on aura mis l'authentification
   devise_for :users
 	root "artworks#index"
 	get 'users/:id', to: "users#show", as: "user_profile"
