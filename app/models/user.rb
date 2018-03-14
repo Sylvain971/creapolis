2984cac6319905294581ee8c88c1dd230d1a79c7
Nico <nico1099@hotmail.fr>
2018-03-14 13:55:19 +0100

    login signup spaces designed font added icons corrected

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  
+
   has_many :pictures
   has_and_belongs_to_many :artworks
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+   def remember_me
+     (super == nil) ? '1' : super
+   end
+
 end
