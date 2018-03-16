01337b98337922980fc2d3897eb4e42c04bfe485
Louis Aurick <louisaurick@ntymail.com>
2018-03-16 11:44:19 +0100

    pictures 1600x900

--- app/uploaders/images_uploader.rb
+++ app/uploaders/images_uploader.rb
@@ -22,7 +22,7 @@ class ImagesUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  	process resize_to_fill: [3200, 1800]
+  	process resize_to_fill: [1600, 900]
 
   #
   # def scale(width, height)
