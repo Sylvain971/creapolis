b1882af46555c6effa4be1521cf860f5b9a2a204
sam28 <sdanfakha@hotmail.fr>
2018-03-16 11:59:07 +0100

    correction bug page show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @picture = Picture.find(params[:id])
+    
 
     @up = @user.pictures
     @up_js = @up.to_json(include: { artwork: { only: [:lat, :long]}})
