a8031b424ec66bdcacde094db468cbd4ab97700d
Louis Aurick <louisaurick@ntymail.com>
2018-03-20 17:47:41 +0100

    all conflicts solved

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180319152524) do
-
 
   create_table "artworks", force: :cascade do |t|
     t.string "title"
