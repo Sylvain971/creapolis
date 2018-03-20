56767a344211068ef6ce9aac0d35ae2d4cb2b39b
Justine <>
2018-03-21 00:09:39 +0100

    added seed for vitry route

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,6 @@
 
 user_admin = User.create(email:"admin@gmail.com", password: "admin2018")
 
-
-
 user_regular = User.create(pseudo: "Nico", email:"nico@gmail.com", password: "coucou")
 user_regular2 = User.create(pseudo: "Samba", email:"samba@gmail.com", password: "coucou")
 user_regular3 = User.create(pseudo: "Justine", email:"justine@gmail.com", password: "coucou")
@@ -34,12 +32,6 @@ picture5 = Picture.create(remote_img_url_url: "http://res.cloudinary.com/creapol
 picture6 = Picture.create(remote_img_url_url: "http://res.cloudinary.com/creapolis/image/upload/v1521038222/image1.jpg", artwork: artwork1, user: user_regular4)
 
 
-artwork1.users << user_dacruz
-
-artwork2.users << user_ren
-
-artwork3.users << user_sanetwo
-
 
 ########### Creation d'une premier itinéraire #########################
 
@@ -53,8 +45,8 @@ artwork13 = Artwork.create(lat:	"48.7940485",	long:"2.3964596", artist: "Stew", 
 artwork14 =Artwork.create(lat:	"48.7934179",	long:"2.3959358", artist: "Roa", style:"Spray")
 artwork15 =Artwork.create(lat:	"48.7980213",	long:"2.39928483", artist: "C215", style:"Pochoir, Spray")
 artwork16 =Artwork.create(lat:	"48.7938571",	long:"2.3993399", artist: "Meushay et Nosbé", style:"Spray")
-artwork17 =Artwork.create(lat:	"48.789351",long:"2.3904885", artist: "MADC", style:"Spray")
-artwork18 = Artwork.create( lat:	"48.790496",	long:"2.388982", artist: "Docteur Bergman", style:"Pochoir")
+#artwork17 =Artwork.create(lat:	"48.789351", long:"2.3904885", artist: "MADC", style:"Spray")
+artwork18 = Artwork.create( lat:	"48.790496", long:"2.388982", artist: "Docteur Bergman", style:"Pochoir")
 artwork19 = Artwork.create( lat:	"48.791128",	long:"2.389571", artist: "Kouka", style:"Acrylique")
 artwork20 = Artwork.create( lat:	"48.7917342",	long:"2.3891235", artist: "Kenji", style:"Spray")
 artwork21 = Artwork.create( lat:	"48.7925282",	long:"2.3962301", artist: "Nychos")
@@ -62,9 +54,9 @@ artwork22 = Artwork.create( lat:	"48.7916606",	long:"2.3897027", artist: "Céles
 artwork23 = Artwork.create( lat:	"48.7914197",	long:"2.3907833", artist: "Takt et Mantra" , style:"Spray")
 artwork24 = Artwork.create( lat:	"48.7904097",	long:"2.3929597", artist: "Astro", style:"Spray, Acrylique")
 artwork25 = Artwork.create( lat:	"48.790398",long:"2.392582", artist: "Hopare, Alex, Brok")
-artwork26 = Artwork.create( lat:	"48.7895618",	long:"2.3927548"	, artist: "exta", style:"Acrylique")
+artwork26 = Artwork.create( lat:	"48.7895618",	long:"2.3927548", artist: "exta", style:"Acrylique")
 artwork27 = Artwork.create( lat:	"48.7937833", long:"2.3987197", artist: "C215", style:"Pochoir")
-artwork28 = Artwork.create( lat:	"48.793686",long:"2.398176", artist: "Fin DAC", style:"Pochoir")
+artwork28 = Artwork.create( lat:	"48.793686", long:"2.398176", artist: "Fin DAC", style:"Pochoir")
 artwork29 = Artwork.create( lat:	"48.788187",	long:"2.39161", artist: "David Walker", style:"Spray")
 artwork30 = Artwork.create( lat:	"48.7884167",	long:"2.3912654", artist: "Philippe Baudeloque", style:"Craie")
 artwork31 = Artwork.create( lat:	"48.7937218",	long:"2.3973139", artist: "Indigo", style:"Pochoir")
@@ -83,7 +75,7 @@ picture13 = Picture.create(remote_img_url_url: "http://res.cloudinary.com/creapo
 picture14 = Picture.create(remote_img_url_url: "http://res.cloudinary.com/creapolis/image/upload/DSC_0754.JPG", user: user_admin, artwork: artwork14)
 picture15 = Picture.create(remote_img_url_url: "http://res.cloudinary.com/creapolis/image/upload/DSC_0733.JPG", user: user_admin, artwork: artwork15)
 picture16 = Picture.create(remote_img_url_url: "http://res.cloudinary.com/creapolis/image/upload/DSC_0746.JPG", user: user_admin, artwork: artwork16)
-picture17 = Picture.create(remote_img_url_url: "http://res.cloudinary.com/creapolis/image/upload/FILE2405.CHK",user: user_admin, artwork: artwork17)
+#picture17 = Picture.create(remote_img_url_url: "http://res.cloudinary.com/creapolis/image/upload/FILE2405.CHK",user: user_admin, artwork: artwork17)
 picture18 = Picture.create(remote_img_url_url: "http://res.cloudinary.com/creapolis/image/upload/DSC_0903.JPG",user: user_admin, artwork: artwork18)
 picture19 = Picture.create(remote_img_url_url: "http://res.cloudinary.com/creapolis/image/upload/DSC_0901.JPG",user: user_admin, artwork: artwork19)
 picture20 = Picture.create(remote_img_url_url: "http://res.cloudinary.com/creapolis/image/upload/DSC_0776.JPG",user: user_admin, artwork: artwork20)
