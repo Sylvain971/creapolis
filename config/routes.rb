066b31a9a49482461989bf8d4eeaaabf151ee456
Louis Aurick <louisaurick@ntymail.com>
2018-03-20 11:04:47 +0100

    algolia

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,10 @@ Rails.application.routes.draw do
 
 	resources :artworks
 	resources :pictures
+	resources :searches
   devise_for :users
 	root "artworks#index"
 	get 'users/:id', to: "users#show", as: "user_profile"
   get 'artworks/show_small/:id', to: "artworks#show_small"
   get 'artworks/new/:lat/:long', to: "arwtorks#new"
-  get 'searches/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
