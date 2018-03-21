279732e10fee4347bbb880447ecd3d01a7860870
Louis Aurick <louisaurick@ntymail.com>
2018-03-21 10:25:07 +0100

    country added to algolia

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,12 +21,12 @@ class User < ApplicationRecord
 	algoliasearch do
 
 		# list of attribute used to build an Algolia record
-    attributes :pseudo, :created_at
+    attributes :pseudo, :artist, :country, :created_at
     # the `searchableAttributes` (formerly known as attributesToIndex) setting defines the attributes
     # you want to search in: here `title`, `subtitle` & `description`.
     # You need to list them by order of importance. `description` is tagged as
     # `unordered` to avoid taking the position of a match into account in that attribute.
-    searchableAttributes ['pseudo']
+    searchableAttributes ['pseudo', 'country']
     # the `customRanking` setting defines the ranking criteria use to compare two matching
     # records in case their text-relevance is equal. It should reflect your record popularity.
 
