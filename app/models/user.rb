e487b559f21687366bd359d73385b420bde1ab0d
Justine <>
2018-03-22 22:35:31 +0100

    added mail to fb oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -72,6 +72,8 @@ class User < ApplicationRecord
       user.firstname = auth.info.first_name   # assuming the user model has a name
       user.name = auth.info.last_name   # assuming the user model has a name
       user.profile_picture = auth.info.image # assuming the user model has an image
+      
+      ConfirmationMailer.sample_email(user).deliver!
     end
 end
 
