c69f36d2ddee919dc53be4397f20d1f8ccf15087
Louis Aurick <louisaurick@ntymail.com>
2018-03-22 11:38:32 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -68,9 +68,9 @@ ActiveRecord::Schema.define(version: 20180321113207) do
     t.string "name"
     t.boolean "artist", default: false
     t.string "profile_picture"
+    t.string "city"
     t.string "provider"
     t.string "uid"
-    t.string "city"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
