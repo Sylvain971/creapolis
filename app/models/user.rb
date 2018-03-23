93e2fe8efd2e56a9110319ff04ec38b0634d0712
Justine <>
2018-03-23 14:05:23 +0100

    added dependent destroy to picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  has_many :pictures
+  has_many :pictures, dependent: :destroy
   has_and_belongs_to_many :created_artworks, class_name: "Artwork"
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
