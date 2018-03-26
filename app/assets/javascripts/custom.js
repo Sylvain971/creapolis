056d0a76f510db4624ec961175a00be43b8f4ee6
Justine <>
2018-03-26 12:30:40 +0200

    changed place of artworks on show user

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,54 +1,47 @@
 $( document ).on('turbolinks:load', function() {
 
 
-$('.user_tabs .tab1').on( 'click', function () {
-      $('.artist_artworks_pictures').hide();
-      $('.user_map').hide();
-      $('.user_pictures').show();
-      $('.user_tabs .tab1').addClass('active_tab');
-      $('.user_tabs li:not(.tab1)').removeClass('active_tab');
-      $(".popup-container").hide();
+  $('.user_tabs .tab1').on( 'click', function () {
+        $('.artist_artworks_pictures').hide();
+        $('.user_map').hide();
+        $('.user_pictures').show();
+        $('.user_tabs .tab1').addClass('active_tab');
+        $('.user_tabs li:not(.tab1)').removeClass('active_tab');
+        $(".popup-container").hide();
 
-      $('.user_tabs').css("height","80px")
-      $('#map_user').css("height","54vh");
-      $('.count-items').show();
-});
+        $('.user_tabs').css("height","80px")
+        $('#map_user').css("height","54vh");
+        $('.count-items').show();
+  });
 
-$('.user_tabs .tab2').on( 'click', function () {
-      $('.artist_artworks_pictures').hide();
-      $('.user_pictures').hide();
-      
-      $('.count-items').hide();
-      $('.user_tabs').css("height","8vh")
-      $('.user_tabs').css("min-height","8vh")
-      $('#map_user').css("height","63vh");
-      $('.user_map').show();
+  $('.user_tabs .tab2').on( 'click', function () {
+        $('.artist_artworks_pictures').hide();
+        $('.user_pictures').hide();
+        
+        $('.count-items').hide();
+        $('.user_tabs').css("height","8vh")
+        $('.user_tabs').css("min-height","8vh")
+        $('#map_user').css("height","63vh");
+        $('.user_map').show();
 
-      $('.user_tabs .tab2').addClass('active_tab');
-      $('.user_tabs li:not(.tab2)').removeClass('active_tab');
-      map._onResize();
+        $('.user_tabs .tab2').addClass('active_tab');
+        $('.user_tabs li:not(.tab2)').removeClass('active_tab');
+        map._onResize();
+  });
 
+  $('.user_tabs .tab0').on( 'click', function () {
+        $('.user_pictures').hide();
+        $('.user_map').hide();
+        $('.artist_artworks_pictures').show();
+        $('.user_tabs .tab0').addClass('active_tab');
+        $('.user_tabs li:not(.tab0)').removeClass('active_tab');
+        $(".popup-container").hide();
 
-});
+        $('.user_tabs').css("height","80px")
+        $('#map_user').css("height","54vh");
+        $('.count-items').show();
 
-$('.user_tabs .tab0').on( 'click', function () {
-      $('.user_pictures').hide();
-      $('.user_map').hide();
-      $('.artist_artworks_pictures').show();
-      $('.user_tabs .tab0').addClass('active_tab');
-      $('.user_tabs li:not(.tab0)').removeClass('active_tab');
-      $(".popup-container").hide();
-
-      $('.user_tabs').css("height","80px")
-      $('#map_user').css("height","54vh");
-      $('.count-items').show();
-
-});
-
-
-
-
-
+  });
 });
 
 
