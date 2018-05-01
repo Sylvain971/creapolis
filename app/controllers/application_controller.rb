b72c55d0f4f63ffaf699f0309439af6fe0069343
Louis Aurick <louisaurick@ntymail.com>
2018-05-01 12:31:24 +0200

    image tag for meta image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :name, :pseudo, :city, :profile_picture])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :name, :pseudo, :city, :profile_picture])
+    devise_parameter_sanitizer.permit(:account_updaste, keys: [:firstname, :name, :pseudo, :city, :profile_picture])
   end
 
 end
